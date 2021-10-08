bf44edbd0429a30ea4d461ec26b3cd098d2d3907
johannes <dev@johanneshuebner.com>
2021-10-08 12:15:37 +0200

    Removed register attribute in printf Allow redefining CAN limits in Makefile (max messages etc.)

--- include/stm32_can.h
+++ include/stm32_can.h
@@ -27,6 +27,19 @@
 #define CAN_ERR_MAXMESSAGES -4
 #define CAN_ERR_MAXITEMS -5
 
+#ifndef MAX_ITEMS_PER_MESSAGE
+#define MAX_ITEMS_PER_MESSAGE    8
+#endif
+#ifndef MAX_MESSAGES
+#define MAX_MESSAGES            10
+#endif
+#ifndef SENDBUFFER_LEN
+#define SENDBUFFER_LEN          20
+#endif
+#ifndef MAX_USER_MESSAGES
+#define MAX_USER_MESSAGES       10
+#endif
+
 class CANIDMAP;
 class SENDBUFFER;
 
@@ -60,10 +73,6 @@ public:
    static Can* GetInterface(int index);
 
 private:
-   static const int MAX_ITEMS_PER_MESSAGE = 8;
-   static const int MAX_MESSAGES = 10;
-   static const int SENDBUFFER_LEN = 20;
-   static const int MAX_USER_MESSAGES = 10;
    static volatile bool isSaving;
 
    struct CANPOS
