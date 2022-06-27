be471282c6532b210e6bf239a21df6d864111496
johannes <dev@johanneshuebner.com>
2022-06-27 14:12:05 +0200

    CAN2 pin mode fix

--- src/stm32_can.cpp
+++ src/stm32_can.cpp
@@ -354,11 +354,6 @@ Can::Can(uint32_t baseAddr, enum baudrates baudrate, bool remap)
             // Configure CAN pin: TX.-
             gpio_set_mode(GPIO_BANK_CAN2_TX, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_CAN2_TX);
          }
-                  // Configure CAN pin: RX (input pull-up).
-         gpio_set_mode(GPIO_BANK_CAN2_RX, GPIO_MODE_INPUT, GPIO_CNF_INPUT_PULL_UPDOWN, GPIO_CAN2_RX);
-         gpio_set(GPIO_BANK_CAN2_RX, GPIO_CAN2_RX);
-         // Configure CAN pin: TX.-
-         gpio_set_mode(GPIO_BANK_CAN2_TX, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_CAN2_TX);
 
          //CAN2 RX and TX IRQs
          nvic_enable_irq(NVIC_CAN2_RX0_IRQ); //CAN RX
