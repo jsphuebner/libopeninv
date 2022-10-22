jsphuebner/libopeninv

commit d5784502adc9c3a240132d62b496a98c9d9cb60f

Fix issues with SDO #3 and #4

src/stm32_can.cpp
@@ -42,6 +42,7 @@
 #define SDO_READ_REPLY        0x43
 #define SDO_ERR_INVIDX        0x06020000
 #define SDO_ERR_RANGE         0x06090030
+#define SDO_ERR_GENERAL       0x08000000
 #define SENDMAP_ADDRESS(b)    b
 #define RECVMAP_ADDRESS(b)    (b + sizeof(canSendMap))
 #define CRC_ADDRESS(b)        (b+ + sizeof(canSendMap) + sizeof(canRecvMap))
@@ -552,35 +553,50 @@ void Can::SDOWrite(uint8_t remoteNodeId, uint16_t index, uint8_t subIndex, uint3
 void Can::ProcessSDO(uint32_t data[2])
 {
    CAN_SDO *sdo = (CAN_SDO*)data;
-   if (sdo->index >= 0x2000 && sdo->index <= 0x2001 && sdo->subIndex < Param::PARAM_LAST)
+
+   if (sdo->index >= 0x2000 && sdo->index <= 0x2100)
    {
       Param::PARAM_NUM paramIdx = (Param::PARAM_NUM)sdo->subIndex;
 
-      //SDO index 0x2001 will lookup the parameter by its unique ID
-      if (sdo->index == 0x2001)
-         paramIdx = Param::NumFromId(sdo->subIndex);
+      //SDO index 0x21xx will look up the parameter by its unique ID
+      //using subIndex as low byte and xx as high byte of ID
+      if ((sdo->index & 0xFF00) == 0x2100)
+         paramIdx = Param::NumFromId(sdo->subIndex + (sdo->index << 8));
 
-      if (sdo->cmd == SDO_WRITE)
+      if (paramIdx < Param::PARAM_LAST)
       {
-         if (Param::Set(paramIdx, sdo->data) == 0)
+         if (sdo->cmd == SDO_WRITE)
          {
-            sdo->cmd = SDO_WRITE_REPLY;
+            if (Param::Set(paramIdx, sdo->data) == 0)
+            {
+               sdo->cmd = SDO_WRITE_REPLY;
+            }
+            else
+            {
+               sdo->cmd = SDO_ABORT;
+               sdo->data = SDO_ERR_RANGE;
+            }
          }
-         else
+         else if (sdo->cmd == SDO_READ)
          {
-            sdo->cmd = SDO_ABORT;
-            sdo->data = SDO_ERR_RANGE;
+            sdo->data = Param::Get(paramIdx);
+            sdo->cmd = SDO_READ_REPLY;
          }
       }
-      else if (sdo->cmd == SDO_READ)
+      else
       {
-         sdo->data = Param::Get(paramIdx);
-         sdo->cmd = SDO_READ_REPLY;
+         sdo->cmd = SDO_ABORT;
+         sdo->data = SDO_ERR_INVIDX;
       }
    }
    else if (sdo->index >= 0x3000 && sdo->index < 0x4800 && sdo->subIndex < Param::PARAM_LAST)
    {
-      if (sdo->cmd == SDO_WRITE)
+      if (isSaving)
+      {
+         sdo->cmd = SDO_ABORT;
+         sdo->data = SDO_ERR_GENERAL;
+      }
+      else if (sdo->cmd == SDO_WRITE)
       {
          int result;
          int offset = sdo->data & 0xFF;

commit 6fe899431d5f3ba80c295ed0e62fbe5378a8b7ec

Fix CANopen SDO support
This fixes a serious protocol bug in the CANopen SDO
support which meant it was not possible to use other
CANopen tools to read and write values. The command
constants for reading and writing were inverted due to the
standard being confusing. In addition some command
transfer flags were incorrect.

It also fixes a bug introduced in commit d5784502 which
prevented the extended dictionary for accessing parameters
by unique ID from operating as intended.

Tests:
 - With a small python script using the "canopen" library verify
    normal operation for reading and writing parameters and
    values:
        print("boost/curkp:", node.sdo.upload(0x2000, 0))
        print("Set boost/curkp to a new value")
        node.sdo.download(0x2000, 0, b'\x00\x04\x00\x00')
        print("boost/curkp:", node.sdo.upload(0x2000, 0))
        print("curkp:", node.sdo.upload(0x2100, 107))
        print("Set curkp to a new value")
        node.sdo.download(0x2100, 107, b'\x01\x02\x00\x00')
        print("curkp:", node.sdo.upload(0x2100, 107))
        print("pot:", node.sdo.upload(0x2107, 0xdf))
 - Verify that an invalid dictionary ID read:
        print("boost/curkp:", node.sdo.upload(0x2001, 0))
 fails with:
   canopen.sdo.exceptions.SdoAbortedError: Code
   0x06020000, Object does not exist
 - Capture CAN packets with Wireshark and verify each
   request and response from the test sequence is decoded
   correctly and matches the expected values and behaviour

src/stm32_can.cpp
@@ -35,11 +35,31 @@
 
 #define MAX_INTERFACES        2
 #define IDS_PER_BANK          4
-#define SDO_WRITE             0x40
-#define SDO_READ              0x22
+
+/*
+ * For details of the SDO protocol see:
+ *
+ * CiA 301 - CANopen application layer and communication profile
+ * Section 7.2.4
+ *
+ * OR
+ *
+ * http://www.byteme.org.uk/canopenparent/canopen/sdo-service-data-objects-canopen/
+ *
+ * In the context of CANopen SDO download is from a client to the server/
+ * inverter and upload is from the server/inverter to the client.
+ */
+#define SDO_REQUEST_DOWNLOAD  (1 << 5)
+#define SDO_REQUEST_UPLOAD    (2 << 5)
+#define SDO_RESPONSE_UPLOAD   (2 << 5)
+#define SDO_RESPONSE_DOWNLOAD (3 << 5)
+#define SDO_EXPEDITED         (1 << 1)
+#define SDO_SIZE_SPECIFIED    (1)
+#define SDO_WRITE             (SDO_REQUEST_DOWNLOAD | SDO_EXPEDITED | SDO_SIZE_SPECIFIED)
+#define SDO_READ              SDO_REQUEST_UPLOAD
 #define SDO_ABORT             0x80
-#define SDO_WRITE_REPLY       0x23
-#define SDO_READ_REPLY        0x43
+#define SDO_WRITE_REPLY       SDO_RESPONSE_DOWNLOAD
+#define SDO_READ_REPLY        (SDO_RESPONSE_UPLOAD | SDO_EXPEDITED | SDO_SIZE_SPECIFIED)
 #define SDO_ERR_INVIDX        0x06020000
 #define SDO_ERR_RANGE         0x06090030
 #define SDO_ERR_GENERAL       0x08000000
@@ -554,14 +574,14 @@ void Can::ProcessSDO(uint32_t data[2])
 {
    CAN_SDO *sdo = (CAN_SDO*)data;
 
-   if (sdo->index >= 0x2000 && sdo->index <= 0x2100)
+   if (sdo->index == 0x2000 || (sdo->index & 0xFF00) == 0x2100)
    {
       Param::PARAM_NUM paramIdx = (Param::PARAM_NUM)sdo->subIndex;
 
       //SDO index 0x21xx will look up the parameter by its unique ID
       //using subIndex as low byte and xx as high byte of ID
       if ((sdo->index & 0xFF00) == 0x2100)
-         paramIdx = Param::NumFromId(sdo->subIndex + (sdo->index << 8));
+         paramIdx = Param::NumFromId(sdo->subIndex + ((sdo->index & 0xFF) << 8));
 
       if (paramIdx < Param::PARAM_LAST)
       {