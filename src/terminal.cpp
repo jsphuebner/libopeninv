fecf2943b2ca44adca25af22dfcf46f32845af0d
johannes <dev@johanneshuebner.com>
2024-04-16 20:39:45 +0200

    ok, ok, just always clear the overrun error whenever it occurs

--- src/terminal.cpp
+++ src/terminal.cpp
@@ -102,6 +102,9 @@ void Terminal::Run()
    int unusedBytes = dma_get_number_of_data(hw->dmactl, hw->dmarx);
    int currentIdx = bufSize - unusedBytes;
 
+   if (usart_get_flag(usart, USART_SR_ORE))
+      usart_recv(usart); //Clear possible overrun
+
    if (0 == unusedBytes)
       ResetDMA();
 
@@ -258,8 +261,6 @@ void Terminal::ResetDMA()
    dma_disable_channel(hw->dmactl, hw->dmarx);
    dma_set_memory_address(hw->dmactl, hw->dmarx, (uint32_t)inBuf);
    dma_set_number_of_data(hw->dmactl, hw->dmarx, bufSize);
-   if (usart_get_flag(usart, USART_SR_ORE))
-      usart_recv(usart); //Clear possible overrun
    dma_enable_channel(hw->dmactl, hw->dmarx);
 }
 
